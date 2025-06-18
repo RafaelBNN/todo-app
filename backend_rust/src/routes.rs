99fd9ebdc7140c8e4bf824ae6705f8aee2771601
Rafael Bernardo <rafaelbnneto@gmail.com>
2025-06-18 11:33:04 -0300

    format routes.rs

--- backend_rust/src/routes.rs
+++ backend_rust/src/routes.rs
@@ -1,16 +1,16 @@
 use axum::routing::{get, put};
 use axum::{http, Extension, Router};
+use http::header::{CONTENT_TYPE, ORIGIN};
 use sqlx::PgPool;
 use tower_http::cors::{Any, CorsLayer};
-use http::header::{CONTENT_TYPE, ORIGIN};
 
-use crate::handlers::{create_todo, list_todos, update_todo, delete_todo};
+use crate::handlers::{create_todo, delete_todo, list_todos, update_todo};
 
 pub fn create_routes(pool: PgPool) -> Router {
     let cors = CorsLayer::new()
-    .allow_origin(Any)
-    .allow_methods(Any)
-    .allow_headers([CONTENT_TYPE, ORIGIN]);
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers([CONTENT_TYPE, ORIGIN]);
 
     Router::new()
         .route("/todos", get(list_todos).post(create_todo))
